96d09a78c8f9ed07ba91d1c04083dc29188ae741
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2019-12-17 23:19:43 +0000

    test for clicking on a produtc

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Product Details", type: :feature, js: true do
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -15,12 +15,13 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They can see the product page" do
     visit root_path
 
     # commented out b/c it's for debugging only
-    # save_and_open_screenshot
-
-    expect(page).to have_css 'article.product', count: 10
+    
+    page.first(".product-name").click
+    find(".product-detail")
+    # save_and_open_screenshot("product-detail.png")
   end
 end
